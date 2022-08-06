8977dc72dd6524690989073f310833cf55dd9c02
藤井真 <fuzimako0202@gmail.com>
2022-08-06 18:17:16 +0900

    logout機能を実装

--- src/components/Logout.tsx
+++ src/components/Logout.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { signInWithPopup } from "firebase/auth";
+import { auth,provider } from '../firebase';
+import { useNavigate } from 'react-router-dom';
 
-const Logout = () => {
+const Logout = (props:any) => {
+  const {setIsAuth} = props;
+  const navigate = useNavigate();
+  const logout = () => {
+    //Googleでログイン
+    signInWithPopup(auth, provider).then((result)=>{
+      //ローカルストレージに認証を残す機能
+      localStorage.setItem("isAuth","true");
+      setIsAuth(true);
+      navigate("/");
+    });
+  };
+
   return (
-    <div>Logout</div>
+    <div>
+      <p>ログインアウトする</p>
+       <button onClick={logout}>ログアウト</button>
+    </div>
   )
 }
 
-export default Logout
+export default Logout;
