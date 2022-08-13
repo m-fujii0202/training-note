18639bf917f0018ad541f3e7ec8b1e987aab8bdd
藤井真 <fuzimako0202@gmail.com>
2022-08-13 17:01:56 +0900

    no message

---
+++ src/components/Menu/DetailedMenu.tsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
+
+const DetailedMenu  = (props:any) => {
+  const {setMenu,menu} = props;
+  
+
+  const onAddMenu = ()=>{
+    console.log("メニューの追加");
+    const newMenu ={
+      menuname:"新しいメニュー"
+    }
+    setMenu(newMenu);
+  }
+
+  return (
+    <SdetailMenu>
+    <SMenuLists>
+      <div className='menuList'>
+        <button>{menu.name}</button>
+      </div>
+
+      <div className='menuList'>
+        <button>{menu.name}</button>
+      </div>
+
+      <div className='addMenu'>
+        <button onClick={onAddMenu}>追加</button>
+      </div>
+    </SMenuLists>
+
+    <SVolumeContainer>
+      <div className='lastTimeVolume'>
+        <p>前回のボリュームです</p>
+      </div>
+
+      <div className='NextTimeVolume'>
+        <p> 目標のボリュームです</p>
+      </div>
+    </SVolumeContainer>
+  </SdetailMenu>
+  )
+}
+
+const SdetailMenu = styled.div`
+  height: 70%;
+`
+
+const SMenuLists = styled.div`
+display: flex;
+flex-direction: column;
+`
+
+const SVolumeContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: flex-end;
+`
+export default DetailedMenu 
---
+++ src/components/Menu/Menus.tsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components';
+import DetailedMenu from './DetailedMenu'
+
+const Menu = () => {
+
+  const [menu, setMenu] = useState([
+    {chest:["ベンチプレス","ダンベルプレス","プッシュアップ"]},
+    {back:["チンニング","デッドリフト","ベントオーバーローイング"]},
+    {leg:["スクワット","レッグプレス","レッグエクステンション"]},
+    {abdomen:["クランチ","レッグレイズ","サイドクランチ"]},
+    {arm:["ダンベルカール","バーベルカール","ハンマーカール"]}
+  ])
+  
+  useEffect(()=>{
+    localStorage.setItem("menu", JSON.stringify(menu));
+  },[menu])
+
+
+const onChangeMenu = ()=>{
+  console.log("部位を選択");
+  const ChangeMenuData = localStorage.getItem("menu");
+  console.log(ChangeMenuData);
+}
+
+
+  return (
+    <SContainer>
+      <div className='title'>
+       <h2>トレーニングメニュー</h2>
+      </div>
+        <div className='menusContainer'>
+          <SBodycontainer>
+            <Sbutton onClick={onChangeMenu} >胸部</Sbutton>
+            <Sbutton>背部</Sbutton>
+            <Sbutton>脚部</Sbutton>
+            <Sbutton>腹部</Sbutton>
+            <Sbutton>腕部</Sbutton>  
+          </SBodycontainer>
+          
+          <DetailedMenu setMenu={setMenu} menu={menu}/>
+
+        </div>
+    </SContainer>
+  )
+}
+
+const SContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 30%
+`
+
+const SBodycontainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+`
+
+const Sbutton = styled.button`
+background-color: aqua;
+padding: 20px;
+`
+
+
+
+export default Menu;
